cd0bc26a62ec6afdf4805aa2486ee94263069de8
Nick Fox-Gieg <nick@fox-gieg.com>
2020-01-07 02:59:03 -0500

    new sevenmove

--- Scripts/SteamVR_SevenMove.cs
+++ Scripts/SteamVR_SevenMove.cs
@@ -2,106 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// https://www.reddit.com/r/Unity3D/comments/a6rmbh/vr_question_steamvr_2_scale_object_with_both_hands/
+
 public class SteamVR_SevenMove : MonoBehaviour {
-    // https://www.youtube.com/watch?v=ycCBzwjOD70
 
     public SteamVR_NewController steamCtlMain;
     public SteamVR_NewController steamCtlAlt;
     public Transform target;
+	public bool clicked = false;
 
-    [Header("Translate")]
-    public bool translateEnabled = true;
-    public float moveSpeed = 4f;
-
-    [Header("Rotate")]
-    public bool rotationEnabled = true;
-    public float rotSpeed = 0.8f;
-
-    [Header("Scale")]
-    public bool scaleEnabled = true;
-    public float minScale = 0.0001f;
-    public float maxScale = Mathf.Infinity;
-    public float scaleTriggerDist = 0.333f;
-    public float scaleSpeed = 0.75f;
-    
-    private Renderer centerRen;
-    private Vector3 prevPosMain = Vector3.zero;
-    private Vector3 prevPosAlt = Vector3.zero;
-    private Vector3 centerPos = Vector3.zero;
-    private float dist = 0f;
-    private float delta = 0f;
-    private float prevDist = 0f;
-
-    private float angle = 0f;
-    private float prevAngle = 0f;
-    private float angleDelta = 0f;
-
-    private float deltaThreshold = 0.01f;
-    private List<Vector3> scaleDeltaSamples = new List<Vector3>();
+    private Vector3 initialHandPosition1; // first controller
+    private Vector3 initialHandPosition2; // second controller
+    private Quaternion initialObjectRotation; // target rotation
+    private Vector3 initialObjectScale; // target scale
+    private Vector3 initialObjectDirection; // direction of target to midpoint of both controllers
 
     private void Update() {
-        dist = (steamCtlMain.transform.position - steamCtlAlt.transform.position).magnitude;
-        delta = dist - prevDist;
-
-        Vector3 deltaPosMain = steamCtlMain.transform.position - prevPosMain;
-        Vector3 deltaPosAlt = steamCtlAlt.transform.position - prevPosAlt;
-        Vector3 deltaPosAvg = (deltaPosMain + deltaPosAlt) / 2f;
-        deltaPosAvg = new Vector3(deltaPosAvg.x, -deltaPosAvg.y, deltaPosAvg.z);
-
-        centerPos = (steamCtlMain.transform.position + steamCtlAlt.transform.position) / 2f;
-
-        if (steamCtlMain.gripped && steamCtlAlt.gripped) {
-            doScale();
-            doRotate();
-            doTranslate(deltaPosAvg);
-            angle = (getAngle(steamCtlMain.transform, centerPos) + getAngle(steamCtlAlt.transform, centerPos)) / 2f;
-        } else if (steamCtlMain.gripped && !steamCtlAlt.gripped) {
-            doTranslate(new Vector3(deltaPosMain.x, -deltaPosMain.y, deltaPosMain.z));
-            angle = getAngle(steamCtlMain.transform, steamCtlMain.transform.forward);// centerPos);
-        } else if (!steamCtlMain.gripped && steamCtlAlt.gripped) {
-            doTranslate(new Vector3(deltaPosAlt.x, -deltaPosAlt.y, deltaPosAlt.z));
-            angle = getAngle(steamCtlAlt.transform, steamCtlAlt.transform.forward);// centerPos);
+        if (clicked) {
+            if (!steamCtlMain.gripped && !steamCtlAlt.gripped) {
+                clicked = false;
+                return;
+            } else {
+                updateTarget();
+            }
+        } else {
+            if (steamCtlMain.gripDown || steamCtlAlt.gripDown) {
+                attachTarget();
+                updateTarget();
+                clicked = true;
+            }
         }
-
-        angleDelta = (angle - prevAngle) * rotSpeed;
-
-        prevPosMain = steamCtlMain.transform.position;
-        prevPosAlt = steamCtlAlt.transform.position;
-        prevDist = dist;
-        prevAngle = angle;
     }
 
-    private float getAngle(Transform t1, Vector3 v1) {
-        Vector3 relative = t1.InverseTransformPoint(v1);
-        float angle = Mathf.Atan2(relative.x, relative.z) * Mathf.Rad2Deg;
-        return angle;
+    private void attachTarget() {
+        initialHandPosition1 = steamCtlMain.transform.position;
+        initialHandPosition2 = steamCtlAlt.transform.position;
+        initialObjectRotation = target.transform.rotation;
+        initialObjectScale = target.transform.localScale;
+        initialObjectDirection = target.transform.position - (initialHandPosition1 + initialHandPosition2) * 0.5f; 
     }
 
-    private void doTranslate(Vector3 deltaPosAvg) {
-        if (!translateEnabled) return;
+    private void updateTarget() {
+        Vector3 currentHandPosition1 = steamCtlMain.transform.position; // current first hand position
+        Vector3 currentHandPosition2 = steamCtlAlt.transform.position; // current second hand position
 
-        Vector3 move = new Vector3(-deltaPosAvg.x, deltaPosAvg.y, -deltaPosAvg.z) * moveSpeed;
-        Vector3 dir = target.InverseTransformDirection(move);
+        Vector3 handDir1 = (initialHandPosition1 - initialHandPosition2).normalized; // direction vector of initial first and second hand position
+        Vector3 handDir2 = (currentHandPosition1 - currentHandPosition2).normalized; // direction vector of current first and second hand position 
 
-        Vector3 finalMove = new Vector3(dir.x, dir.y, dir.z);
-        target.Translate(finalMove);
-    }
+        Quaternion handRot = Quaternion.FromToRotation(handDir1, handDir2); // calculate rotation based on those two direction vectors
+        
+        float currentGrabDistance = Vector3.Distance(currentHandPosition1, currentHandPosition2);
+        float initialGrabDistance = Vector3.Distance(initialHandPosition1, initialHandPosition2);
+        float p = (currentGrabDistance / initialGrabDistance); // percentage based on the distance of the initial positions and the new positions
 
-    private void doRotate() {
-        if (!rotationEnabled) return;
+        Vector3 newScale = new Vector3(p * initialObjectScale.x, p * initialObjectScale.y, p * initialObjectScale.z); // calculate new object scale with p
 
-        target.rotation = Quaternion.RotateTowards(target.rotation, Quaternion.Euler(target.localEulerAngles.x, target.localEulerAngles.y - angleDelta, target.localEulerAngles.z), 1f);
-    }
-
-    private void doScale() {
-        if (!scaleEnabled) return;
-
-        if (Mathf.Abs(delta) > Mathf.Abs(deltaThreshold * target.localScale.x)) {
-            Vector3 scaleDelta = Vector3.one * Mathf.Sign(delta); 
-            Vector3 newScale = target.localScale - scaleDelta;
-            newScale = new Vector3(Mathf.Clamp(newScale.x, minScale, maxScale), Mathf.Clamp(newScale.y, minScale, maxScale), Mathf.Clamp(newScale.z, minScale, maxScale));
-            target.localScale = Vector3.Lerp(target.localScale, newScale, scaleSpeed / 100f);
-        }
+        target.transform.rotation = handRot * initialObjectRotation; // add rotation
+        target.transform.localScale = newScale; // set new scale
+        
+        // set the position of the object to the center of both hands based on the original object direction relative to the new scale and rotation
+        target.transform.position = (0.5f * (currentHandPosition1 + currentHandPosition2)) + (handRot * (initialObjectDirection * p));
     }
 
 }
